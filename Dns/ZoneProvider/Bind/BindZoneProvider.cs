63b6d54741c4ab60e88bb44bd837697afe37dba7
stephbu@microsoft.com <stephbu@microsoft.com>
2015-09-11 12:40:12 -0700

    Pseudo code zone parser

--- Dns/ZoneProvider/Bind/BindZoneProvider.cs
+++ Dns/ZoneProvider/Bind/BindZoneProvider.cs
@@ -17,6 +17,21 @@ namespace Dns.ZoneProvider.Bind
 
         public override Zone GenerateZone()
         {
+            // RFC 1035 - https://tools.ietf.org/html/rfc1035
+            // Forward scanning parser
+            // while(not EOF)
+            //    State is in record
+            //    General Field list : Name Class Type [(Data 0..*)] EOR
+            //    $ORIGIN [name]
+            //    $TTL Timespan
+            //    [Name|@] IN SOA Name
+            //    [Name|@] IN NS Name
+            //    [Name|@] IN MX Priority Name
+            //    [Name|@] IN A IPv4
+            //    [Name|@] IN AAAA IPv6
+            //    [Name|@] IN CNAME name
+            // endwhile
+
             throw new NotImplementedException();
         }
 
